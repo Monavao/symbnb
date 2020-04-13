22fc8c2cd878d475b25bad21fea82da2c3dde4cc
monavao <omid.monavari@hotmail.fr>
2020-04-13 18:43:34 +0200

    Refacto formulaires

--- src/Form/ImageType.php
+++ src/Form/ImageType.php
@@ -14,7 +14,8 @@ class ImageType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('urlFile', FileType::class, ['attr' => ['placeholder' => 'URL de l\'image'], 'required' => false])
+            ->add('urlFile', FileType::class, ['attr' => ['placeholder' => 'URL de l\'image'], 'required' => false/*, 'label' => false*/])
+//            ->add('url', TextType::class)
             ->add('caption', TextType::class, ['attr' => ['placeholder' => 'Titre de l\'image'], 'required' => true]);
     }
 
