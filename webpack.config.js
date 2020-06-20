cc6c42d46a0645881c9f4bae1922f153ce91886a
monavao <omid.monavari@hotmail.fr>
2020-06-21 01:52:11 +0200

    Migration script Js de booking.html.twig vers booking.js

--- webpack.config.js
+++ webpack.config.js
@@ -26,6 +26,7 @@ Encore
     .addEntry('app', './assets/js/app.js')
     .addEntry('datepickerjs', './assets/js/datepicker.js')
     .addEntry('ad', './assets/js/adScript.js')
+    .addEntry('booking', './assets/js/booking.js')
     //.addEntry('page2', './assets/js/page2.js')
 
     // When enabled, Webpack "splits" your files into smaller pieces for greater optimization.
